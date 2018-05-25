453607110c754f876d12cf2da18dc075d8ecebd0
Md Noorshid <mdnoorshid254@gmail.com>
2018-05-25 14:26:34 +0530

    Jacoco with spring boot

---
+++ JacocoDemoWithSpringBoot/src/main/java/com/reactiveworks/jacoco/entity/Employee.java
@@ -0,0 +1,35 @@
+package com.reactiveworks.jacoco.entity;
+
+public class Employee {
+private Integer id;
+private String name;
+private String designation;
+public Employee(Integer id, String name, String designation) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.designation = designation;
+}
+
+public Employee() {
+}
+
+public Integer getId() {
+	return id;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getDesignation() {
+	return designation;
+}
+public void setDesignation(String designation) {
+	this.designation = designation;
+}
+}
